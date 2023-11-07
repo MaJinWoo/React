aa392525a60ced757cf07d28d8668d6b4729c938
Majinwoo <maddonge@naver.com>
2023-11-07 16:17:56 +0900

    버튼 기능 구현

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -5,47 +5,73 @@ function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [todo, setTodo] = useState([
-    {
-      id : 1,
-      title : '제목1',
-      content : '내용1',
-      isDone : false,
-    },
-    {
-      id : 2,
-      title : '제목2',
-      content : '내용2',
-      isDone : true,
-    },
-  ]);
+      {
+        id : 1,
+        title : '제목1',
+        content : '내용1',
+        isDone : false,
+      },
+      {
+        id : 2,
+        title : '제목2',
+        content : '내용2',
+        isDone : true,
+      },
+    ]);
+  
+  const addTodo = function(){
+      const newTodo={
+        id:(todo.length+1),
+        title,
+        content,
+        isDone:false,
+      }
+      setTodo([...todo, newTodo]);
+      setTitle('');
+      setContent('');
+    
+  };
   const todoList = todo.filter((todo)=>{
     return todo.isDone === false;
   });
   const completeList = todo.filter((todo)=>{
     return todo.isDone === true;
   })
-  const addTodo = function(){
-    const newTodo={
-      id:(todo.length+1),
-      title,
-      content,
-      isDone:false,
-    }
-    setTodo([...todo, newTodo]);
+
+
+  const changeStatus=function(id){
+    const changeStatus = todo.map((item)=>{
+      if(item.id===id){
+        return{
+          ...item, 
+          isDone : !item.isDone,
+        }
+      }else{
+        return item;
+      }
+    });
+    setTodo(changeStatus);
   }
 
+
+  const deleteTodo = function(id){
+    const deletedTodo = todo.filter((item)=>{
+      return item.id !== id;
+    })
+    setTodo(deletedTodo);
+  }
   return (
     <div>
       <header>ToDo List</header>
       <main>
-        <div>
-          <input value = {title} onChange={(e)=>{
-            setTitle(e.target.value);
-          }}/>
-          <input value = {content} onChange={(e)=>{
-            setContent(e.target.value);
-          }}/>
-          <button onClick={addTodo}>입력</button>
+      <div>
+            <input value = {title} onChange={(e)=>{
+                    setTitle(e.target.value);
+                }}/>
+            <input value = {content} onChange={(e)=>{
+                    setContent(e.target.value);
+                }}/>
+            <button onClick={addTodo}>입력</button>
         </div>
         <div>
           <h2>할 일</h2>
@@ -55,8 +81,8 @@ function App() {
               <p>{todo.id}</p>
               <p>{todo.title}</p>
               <p>{todo.content}</p>
-              <button >완료</button>
-              <button >삭제</button>
+              <button onClick={(e)=>{changeStatus(todo.id,e)}}>완료</button>
+              <button onClick={(e)=>{deleteTodo(todo.id,e)}}>삭제</button>
               </div>
             );
           })}
@@ -69,6 +95,8 @@ function App() {
               <p>{todo.id}</p>
               <p>{todo.title}</p>
               <p>{todo.content}</p>
+              <button onClick={(e)=>{changeStatus(todo.id,e)}}>취소</button>
+              <button onClick={(e)=>deleteTodo(todo.id, e)}>삭제</button>
               </div>
             );
           })}
@@ -77,6 +105,6 @@ function App() {
       <footer>푸터</footer>
     </div>
   );
-}
 
+        }
 export default App;
