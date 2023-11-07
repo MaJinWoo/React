328297d6c5500a46596175151e5ffc8626dcf151
Majinwoo <maddonge@naver.com>
2023-11-07 11:40:54 +0900

    todoList

---
+++ todo/src/App.jsx
@@ -0,0 +1,82 @@
+import {useState} from "react";
+import './App.css';
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [todo, setTodo] = useState([
+    {
+      id : 1,
+      title : '제목1',
+      content : '내용1',
+      isDone : false,
+    },
+    {
+      id : 2,
+      title : '제목2',
+      content : '내용2',
+      isDone : true,
+    },
+  ]);
+  const todoList = todo.filter((todo)=>{
+    return todo.isDone === false;
+  });
+  const completeList = todo.filter((todo)=>{
+    return todo.isDone === true;
+  })
+  const addTodo = function(){
+    const newTodo={
+      id:(todo.length+1),
+      title,
+      content,
+      isDone:false,
+    }
+    setTodo([...todo, newTodo]);
+  }
+
+  return (
+    <div>
+      <header>ToDo List</header>
+      <main>
+        <div>
+          <input value = {title} onChange={(e)=>{
+            setTitle(e.target.value);
+          }}/>
+          <input value = {content} onChange={(e)=>{
+            setContent(e.target.value);
+          }}/>
+          <button onClick={addTodo}>입력</button>
+        </div>
+        <div>
+          <h2>할 일</h2>
+          {todoList.map((todo)=>{
+            return (
+              <div key = {todo.id}>
+              <p>{todo.id}</p>
+              <p>{todo.title}</p>
+              <p>{todo.content}</p>
+              <button >완료</button>
+              <button >삭제</button>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <h2>완료된 일</h2>
+          {completeList.map((todo)=>{
+            return (
+              <div key = {todo.id}>
+              <p>{todo.id}</p>
+              <p>{todo.title}</p>
+              <p>{todo.content}</p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+      <footer>푸터</footer>
+    </div>
+  );
+}
+
+export default App;
